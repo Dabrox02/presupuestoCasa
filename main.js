9672b1cb0ec04b9304f25aa85e9535206b0c12a1
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-09-19 22:55:09 -0500

    feat: :sparkles: put request done
    - Add modal dialog to edit
    - Put request done

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import { mostrarPagina, paginaAnterior, paginaSiguiente } from "./module/paginac
 
 const d = document;
 const form = d.querySelector("#frm-caja");
+const form_edit = d.querySelector("#frm-edit");
+const modal_edit = d.querySelector("#modal-edit");
+const btn_close_modal = d.querySelectorAll(".close-modal");
 const table = d.querySelector(".tabla-movimientos");
 const btn_prev = d.querySelector("#btn-prev");
 const btn_next = d.querySelector("#btn-next");
@@ -33,6 +36,22 @@ addEventListener("DOMContentLoaded", async (e) => {
         })
     })
 
+    btn_close_modal.forEach((e) => {
+        e.addEventListener("click", (j) => {
+            const dialogo = j.target.closest("dialog");
+            if (dialogo) modal_edit.close();
+            form_edit.removeAttribute("data-edit");
+        })
+    })
+
+    btns_edit.forEach((e) => {
+        let valor = e.dataset.edit;
+        e.addEventListener("click", async (j) => {
+            form_edit.dataset.edit = e.dataset.edit;
+            modal_edit.showModal();
+        })
+    })
+
     filas = document.querySelectorAll(".fila");
     table_config.max_page = Math.ceil(filas.length / table_config.length);
     mostrarPagina(filas, table_config.current_page, table_config.length);
@@ -50,6 +69,20 @@ form.addEventListener("submit", async (e) => {
     }
 })
 
+form_edit.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    config["method"] = "PUT";
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    if (!isNaN(Number(data.valor))) {
+        config["body"] = JSON.stringify(data);
+        if (!form_edit.dataset.edit) return;
+        let res = await fetch(`https://6509d045f6553137159c106b.mockapi.io/Prespuesto/${form_edit.dataset.edit}`, config);
+        window.location.reload();
+    }
+})
+
+
 btn_prev.addEventListener("click", (e) => {
     paginaAnterior(table_config);
     mostrarPagina(filas, table_config.current_page, table_config.length);
@@ -58,4 +91,3 @@ btn_next.addEventListener("click", (e) => {
     paginaSiguiente(table_config);
     mostrarPagina(filas, table_config.current_page, table_config.length);
 });
-

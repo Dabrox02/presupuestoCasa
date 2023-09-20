4454bbc4b25b312cb64632008b52278f8da2390b
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-09-19 19:13:24 -0500

    feat: :sparkles: delete request done

--- main.js
+++ main.js
@@ -3,24 +3,28 @@ import { createTable } from "./module/tablePresupuesto.js";
 const d = document;
 const form = d.querySelector("#frm-caja");
 const table = d.querySelector(".tabla-movimientos");
+let config = {
+    headers: { "content-type": "application/json" },
+}
 
 addEventListener("DOMContentLoaded", async (e) => {
     let res = await (await fetch("https://6509d045f6553137159c106b.mockapi.io/Prespuesto")).json();
     let tbody = createTable(res);
     table.insertAdjacentElement("beforeend", tbody);
+    let btns = d.querySelectorAll(".del-caja");
+
+    btns.forEach((e) => {
+        config["method"] = "DELETE";
+        let valor = e.dataset.del;
+        e.addEventListener("click", async (e) => {
+            res = await (await fetch(`https://6509d045f6553137159c106b.mockapi.io/Prespuesto/${valor}`, config)).json();
+            window.location.reload();
+        })
+    })
 })
 
 
 form.addEventListener("submit", (e) => {
 
-})
-
-const btns = d.querySelectorAll("del-caja");
-console.log(d);
-
-btns.forEach((e) => {
-    e.addEventListener("click", (e) => {
-        console.log("hola");
-    })
 })
 
